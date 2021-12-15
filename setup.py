34697c7492b5d84803388c564b3dd3162110e5fb
Johannes Englisch <englisch@shh.mpg.de>
2021-12-15 10:51:46 +0100

    update to collkey-less clld

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'clldmpg>=4.2',
-        'clld>=8',
+        'clld>=9',
         'clld-glottologfamily-plugin>=4.0',
         'gitpython',
         'transliterate',
